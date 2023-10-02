974830795a826ba6762689fe45c1fafb588dafb1
pp0kt <96444250+pp0kt@users.noreply.github.com>
2023-10-02 12:19:22 +0100

    #18 change strcuture of root moduel and add tagging (#22)

--- main.tf
+++ main.tf
@@ -1,23 +1,4 @@
 # this is my first comment that I'm going to commit
-terraform {
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-        aws = {
-      source = "hashicorp/aws"
-      version = "5.17.0"
-    }
-  }
-}
-
-provider "aws" {
-  # Configuration options
-}
-provider "random" {
-  # Configuration options
-}
 
 #https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 resource "random_string" "bucket_name" {
@@ -32,8 +13,10 @@ resource "aws_s3_bucket" "example" {
   # https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
   bucket = random_string.bucket_name.result
 
+
+tags = {
+    UserUuid     = var.user_uuid
+     }
 }
 
-output "random_bucket_name" {
-    value = random_string.bucket_name.result
-    }
+
